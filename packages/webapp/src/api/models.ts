518af100231c39ceeb453c04d1af098632c70705
underwater <fares.eidi@gmail.com>
2024-04-02 07:41:50 +0200

    Initialize chore/refactor-chat-controller

---
+++ packages/webapp/src/api/models.ts
@@ -0,0 +1,11 @@
+export const enum Approaches {
+  RetrieveThenRead = 'rtr',
+  ReadRetrieveRead = 'rrr',
+  ReadDecomposeAsk = 'rda',
+}
+
+export const enum RetrievalMode {
+  Hybrid = 'hybrid',
+  Vectors = 'vectors',
+  Text = 'text',
+}
